3636e55edc756a79cad1c32a8c1820c171908bca
jkjan <jkjan@naver.com>
2020-01-15 17:58:48 +0900

    firebase messaging

--- app/src/main/java/com/jun/clover/activity/MainActivity.kt
+++ app/src/main/java/com/jun/clover/activity/MainActivity.kt
@@ -2,8 +2,12 @@ package com.jun.clover.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
 import com.jun.clover.R
 import com.jun.clover.viewmodel.MainViewModel
 import com.jun.clover.databinding.ActivityMainBinding
@@ -22,5 +26,18 @@ class MainActivity : BaseActivity() {
         binding.lifecycleOwner = this
         binding.mvm = mMainViewModel
         startService(Intent(this, LockScreenService::class.java))
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.d("firebase token fail", task.exception.toString())
+                    return@OnCompleteListener
+                }
+                // get new instance id token
+                val token = task.result?.token
+                Log.d("firebase token ok", token)
+            })
+
+        FirebaseMessaging.getInstance().isAutoInitEnabled = true
     }
 }
--- app/src/main/java/com/jun/clover/firebase/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/jun/clover/firebase/MyFirebaseMessagingService.kt
@@ -1,4 +1,48 @@
 package com.jun.clover.firebase
 
-class MyFirebaseMessagingService {
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.jun.clover.R
+import com.jun.clover.activity.MainActivity
+import org.koin.core.logger.KOIN_TAG
+import retrofit2.http.Body
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        Log.d("new firebase token", "Refreshed token : $token")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.notification != null) {
+            Log.d("firebase message", "Notification Message : ${remoteMessage.notification?.body}")
+            sendNotification(remoteMessage.notification?.body)
+        }
+    }
+
+    private fun sendNotification(body: String?) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            putExtra("Notification", body)
+        }
+
+        var pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        var notificationBuilder = NotificationCompat.Builder(this, "Notification")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Push Notification FCM")
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSound(notificationSound)
+            .setContentIntent(pendingIntent)
+
+        var notificationManager : NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
+    }
 }
